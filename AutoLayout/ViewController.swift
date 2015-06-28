0e4d0663772834e58a56c10a65212f86e9e34564
Zheng Hao Tan <tanzhao@umich.edu>
2015-06-28 18:36:21 -0500

    Added outlets and actions to the main login page

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -10,7 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+ 
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    var secure: Bool = false { didSet { updateUI() } }
 
-
+    private func updateUI() {
+        passwordField.secureTextEntry = secure
+        passwordLabel.text = secure ? "Secured Password" : "Password"
+    }
+    @IBAction func toggleSecurity() {
+        secure = !secure
+    }
 }
 
